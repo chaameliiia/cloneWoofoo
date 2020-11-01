2ed8fdf9d818ed205ec180b526f0cd022b168d66
chaemi Lee <chaemilee9012@gmail.com>
2020-11-01 13:29:50 +0900

    update: 슬라이드 nextBtn 기능 구현 완료
    - prevBtn 기능 구현 중
    
    2_3

--- src/components/index/ReviewPages.js
+++ src/components/index/ReviewPages.js
@@ -14,43 +14,46 @@ const ReviewPageStyled = styled.ul`
   .review__contents__page {
     left: 0;
     position: absolute;
+    transition: 0.5s;
     width: 100%;
     top: 0;
-/* 
+
     &:nth-of-type(2) {
       left: 100%;
     }
-    
+
     &:nth-of-type(3) {
       left: 100%;
     }
-    
+
     &:nth-of-type(4) {
       left: 100%;
     }
-    
+
     &:nth-of-type(5) {
       left: 100%;
     }
-    
+
     &:nth-of-type(6) {
       left: 100%;
     }
-    
+
     &:nth-of-type(7) {
       left: 100%;
-    } */
+    }
 
     &.prev {
       left: -100%;
+    }
+
+    &.active {
+      left: 0;
     }
 
     &.next {
       left: 100%;
     }
   }
-
-  
 `;
 
 const ReviewPages = () => {
@@ -58,69 +61,69 @@ const ReviewPages = () => {
   const pageCount = useRef(0);
   let pageWrapper, pageChildren;
 
-  useEffect(() => {
-    const pageLists = document.querySelectorAll('.review__contents__page');
-    for(let i = 1; i < pageLists.length; i++) {
-      pageLists[i].classList.add('next');
-    };
-
-    for(let i = 0; i < pageLists.length; i++) {
-      pageLists[i].style = "transition: .5s";
-    }
-  }, []);
+  // useEffect(() => {
+  //   const pageFirst = document.querySelector('.review__contents').firstChild;
+  //   pageFirst.classList.add('active');
+  // },[]);
 
   const nextBtn = () => {
     pageWrapper = document.querySelector('.review__contents');
     pageChildren = pageWrapper.children;
     pageCount.current += 1;
-    if(pageCount.current > pageWrapper.length - 1) {
-      // li page보다 i 커질 경우
+    if (pageCount.current === pageChildren.length) {
+      // i값 초기화
       pageCount.current = 0;
-      for(let i = 1; i < pageWrapper.length; i++) {
-        pageChildren[pageCount.current].classList.add('next')
+      for (let i = 0; i < pageChildren.length - 1; i++) {
+        pageChildren[i + 1].classList.remove('prev');
       }
     }
-    console.log('next', pageCount.current);
 
-    // for(let i = 0; i < pageChildren.length; i++) {
-    //   pageChildren[i].classList.remove('selected');
-    // }
-    pageChildren[pageCount.current].classList.remove('next');
-    
-    if(pageCount.current !== 0) {
+    for (let i = 0; i < pageChildren.length; i++) {
+      pageChildren[i].classList.remove('active');
+    }
+
+    pageChildren[pageCount.current].classList.add('active');
+
+    if (pageCount.current > 0) {
+      // 페이지 번호 1 이상일 때
       pageChildren[pageCount.current - 1].classList.add('prev');
     }
   };
 
   const prevBtn = () => {
-    if(pageCount.current < 0) {
-      pageCount.current = reviews.length - 1;
-    }
+    pageWrapper = document.querySelector('.review__contents');
+    pageChildren = pageWrapper.children;
     pageCount.current -= 1;
+    if (pageCount.current < 0) {
+      // i값 초기화
+      pageCount.current = pageChildren.length - 1;
+    }
     console.log('prev', pageCount.current);
+
+    for (let i = pageCount.current; i < 0; i--) {
+      pageChildren[i].classList.remove('active');
+    }
+
+    pageChildren[pageCount.current].classList.add('active');
+
+    if (pageCount.current < pageChildren.length - 1) {
+      pageChildren[pageCount.current - 1].classList.add('next');
+    }
   };
-  
+
   return (
     <>
       <ReviewPageStyled className="review__contents">
         {reviews.map((v, i) => {
-          return(
-            <li
-              key={i}
-              className="review__contents__page"
-            >
+          return (
+            <li key={i} className="review__contents__page">
               {i}
-              <ReviewDetails
-                reviews={v}
-              />
+              <ReviewDetails reviews={v} />
             </li>
-          )
+          );
         })}
       </ReviewPageStyled>
-      <ReviewButtons
-        clickPrev={prevBtn}
-        clickNext={nextBtn}
-      />
+      <ReviewButtons clickPrev={prevBtn} clickNext={nextBtn} />
     </>
   );
 };
