15c05daef3305807b0fe53c599195e9bb900d842
chaemi Lee <chaemilee9012@gmail.com>
2020-11-06 15:59:58 +0900

    update: 슬라이드 기능 수정 중
    - 슬라이드 버튼 눌렀을 때 화면 바뀌는 기능 수정 중
    
    2_3_1

--- src/components/index/ReviewPages.js
+++ src/components/index/ReviewPages.js
@@ -7,107 +7,115 @@ import ReviewButtons from './ReviewButtons';
 const ReviewPageStyled = styled.ul`
   min-height: 350px;
   overflow: hidden;
-  padding: 8rem;
   position: relative;
   width: 100%;
 
   .review__contents__page {
     left: 0;
     position: absolute;
-    transition: 0.5s;
+    /* transition: 0.5s; */
     width: 100%;
     top: 0;
 
-    &:nth-of-type(2) {
-      left: 100%;
+    /* &:nth-of-type(2) {
+      left: calc(100% * 1);
     }
 
     &:nth-of-type(3) {
-      left: 100%;
+      left: calc(100% * 2);
     }
 
     &:nth-of-type(4) {
-      left: 100%;
+      left: calc(100% * 3);
     }
 
     &:nth-of-type(5) {
-      left: 100%;
+      left: calc(100% * 4);
     }
 
     &:nth-of-type(6) {
-      left: 100%;
+      left: calc(100% * 5);
     }
 
     &:nth-of-type(7) {
-      left: 100%;
-    }
+      left: calc(100% * 6);
+    } */
 
     &.prev {
       left: -100%;
+      transition: 0.5s;
     }
 
     &.active {
       left: 0;
+      transition: 0.5s;
     }
 
     &.next {
       left: 100%;
+      transition: 0.5s;
     }
   }
 `;
 
 const ReviewPages = () => {
   const reviews = useContext(ReviewContext);
-  const pageCount = useRef(0);
+  let pageCount = useRef(0);
+  let bln = true;
   let pageWrapper, pageChildren;
 
-  // useEffect(() => {
-  //   const pageFirst = document.querySelector('.review__contents').firstChild;
-  //   pageFirst.classList.add('active');
-  // },[]);
+  const setPageDefault = () => {
+    pageWrapper = document.querySelector('.review__contents');
+    pageChildren = pageWrapper.children;
+    pageWrapper.firstChild.style = 'left: 0';
+
+    for (let i = 1; i < pageChildren.length; i++) {
+      pageChildren[i].style = `left: calc(${i} * 100%);`;
+    }
+    pageWrapper.lastChild.style = 'left: -100%';
+  };
+
+  setTimeout(() => {
+    setPageDefault();
+  }, 10);
 
   const nextBtn = () => {
     pageWrapper = document.querySelector('.review__contents');
     pageChildren = pageWrapper.children;
-    pageCount.current += 1;
-    if (pageCount.current === pageChildren.length) {
-      // i값 초기화
-      pageCount.current = 0;
-      for (let i = 0; i < pageChildren.length - 1; i++) {
-        pageChildren[i + 1].classList.remove('prev');
+
+    if (bln === true) {
+      bln = false;
+
+      if (pageCount.current > pageChildren.length) {
+        // 페이지 값 초기화
+        pageCount.current = 0;
       }
-    }
+      pageCount.current++;
+      pageWrapper.append(pageWrapper.firstChild);
+      setPageDefault();
 
-    for (let i = 0; i < pageChildren.length; i++) {
-      pageChildren[i].classList.remove('active');
-    }
-
-    pageChildren[pageCount.current].classList.add('active');
-
-    if (pageCount.current > 0) {
-      // 페이지 번호 1 이상일 때
-      pageChildren[pageCount.current - 1].classList.add('prev');
+      setTimeout(() => {
+        bln = true;
+      }, 300);
     }
   };
 
   const prevBtn = () => {
     pageWrapper = document.querySelector('.review__contents');
     pageChildren = pageWrapper.children;
-    pageCount.current -= 1;
-    if (pageCount.current < 0) {
-      // i값 초기화
-      pageCount.current = pageChildren.length - 1;
-    }
-    console.log('prev', pageCount.current);
 
-    for (let i = pageCount.current; i < 0; i--) {
-      pageChildren[i].classList.remove('active');
-    }
+    if (bln === true) {
+      bln = false;
+      if (pageCount.current < 0) {
+        pageCount.current = pageChildren.length - 1;
+      }
+      pageCount.current--;
+      pageWrapper.prepend(pageWrapper.lastChild);
+      setPageDefault();
 
-    pageChildren[pageCount.current].classList.add('active');
-
-    if (pageCount.current < pageChildren.length - 1) {
-      pageChildren[pageCount.current - 1].classList.add('next');
+      setTimeout(() => {
+        bln = true;
+      }, 300);
     }
   };
 
